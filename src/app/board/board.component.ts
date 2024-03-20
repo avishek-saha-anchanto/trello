429ec8bf92226963a8f66da3865a4f2e0598fe5d
Avishek <avishek.saha@anchanto.com>
2024-03-20 14:56:39 +0530

    feat:Add card
    Add card, create a card model, add Modal to Edit card

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,31 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Board } from '../board.model';
 import { List } from '../list.model';
 import { BoardService } from '../service/board.service';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BoardComponent implements OnInit {
-  board: Board ;
-
-  constructor(private boardService:BoardService ){}
+  boards=["Intern","my project","Test"];
+  board: Board;
+  isAdd: boolean = false;
+  @ViewChild('inputField') inputField: ElementRef;
+  cardName: string;
+  addingToListIndex: number = -1;
+  constructor(private boardService: BoardService) {}
   ngOnInit() {
-    this.board=this.boardService.getBoard();
+    this.board = this.boardService.getBoard();
   }
-  
+
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
+      moveItemInArray(
         event.container.data,
         event.previousIndex,
-        event.currentIndex);
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     }
   }
 
+  addCard(cardName: string, listIndex: number) {
+    if(cardName.length==0)
+    return;
+    if (this.isAdd && this.addingToListIndex === listIndex) {
+      // console.log(
+      //   'Adding card to list:',
+      //   listIndex,
+      //   'with title:',
+      //   this.cardTitle
+      // );
+      this.isAdd = false;
+      this.addingToListIndex = -1;
+    } else {
+      this.addingToListIndex = listIndex;
+      this.isAdd = !this.isAdd;
+    }
+    this.boardService.addCardOnBoard(listIndex, cardName);
+    this.inputField.nativeElement.value = '';
+  }
 }
