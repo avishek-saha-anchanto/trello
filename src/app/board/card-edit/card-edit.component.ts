5a48c0ac8b34d1b78d097524bc07a0c24c21c34c
Avishek <avishek.saha@anchanto.com>
2024-03-22 16:36:16 +0530

    fix: Bug fix of edit button

--- src/app/board/card-edit/card-edit.component.ts
+++ src/app/board/card-edit/card-edit.component.ts
@@ -1,20 +1,55 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BoardService } from '../../service/board.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card-edit',
   templateUrl: './card-edit.component.html',
   styleUrl: './card-edit.component.scss',
 })
-export class CardEditComponent {
+export class CardEditComponent implements OnInit {
   @Input() isModalEdit;
+  @Input() index: number;
+  @Input() listName: string;
+  @Input() listIndex: number;
+  bindex: number;
 
-  constructor(boardService:BoardService){}
-  closeModal() {
-    this.isModalEdit = !this.isModalEdit;
+  newTitle: string = '';
+  description: string = '';
+
+  constructor(
+    private boardService: BoardService,
+    private route: ActivatedRoute
+  ) {}
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const boardIndex = +params['id'];
+      this.bindex = boardIndex;
+    });
   }
-  
-  onSave() {
+  closeModal() {
+    this.isModalEdit = false;
+  }
 
+  handleOverlayClick(event: MouseEvent) {
+    if (event.target === event.currentTarget) {
+      this.closeModal();
+    }
+  }
+  handleContentClick(event: MouseEvent) {
+    event.stopPropagation(); 
+  }
+
+  onSave() {
+    this.boardService.editCardName(
+      this.index,
+      this.newTitle,
+      this.listIndex,
+      this.bindex,
+      this.description
+    );
+    this.closeModal();
+    this.newTitle = '';
+    this.description = '';
   }
 }
